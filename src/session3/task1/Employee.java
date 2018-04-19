58cfb9aac167fc232644e05286cd70d93f3204b3
Darko Martinovikj <Darko.Martinovikj@endava.com>
2018-04-19 16:59:47 +0200

    changes in Employee

--- src/session3/task1/Employee.java
+++ src/session3/task1/Employee.java
@@ -4,10 +4,15 @@ public class Employee {
 
     String name;
     int age;
+    String surname;
 
     public Employee(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String toString() {
