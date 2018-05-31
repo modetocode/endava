3e0d0026bd5b42ff76f5c23778bed93402e311e5
Darko Martinovikj <Darko.Martinovikj@endava.com>
2018-05-31 17:00:15 +0200

    changed main in session 1

--- src/session1/Main.java
+++ src/session1/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int productPrice = 200;
-        int discountPercentage = 10;
+        int productPrice = 250;
+        int discountPercentage = 15;
         float productPriceAfterDiscount = productPrice - productPrice * (discountPercentage / 100.0f);
         System.out.println(productPriceAfterDiscount);
     }
