3e6c1c4070dcc161174043c5341ed9efb13ae420
Darko Martinovikj <Darko.Martinovikj@endava.com>
2018-05-31 17:36:33 +0200

    changed the name

--- src/session1/Main.java
+++ src/session1/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int productPrice = 270;
+        int price = 270;
         int discountPercentage = 15;
-        float productPriceAfterDiscount = productPrice - productPrice * (discountPercentage / 100.0f);
+        float productPriceAfterDiscount = price - price * (discountPercentage / 100.0f);
         System.out.println(productPriceAfterDiscount);
         System.out.println("Hello");
     }
